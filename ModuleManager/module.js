88bd0d3d38d64a0175ac81aa1bde474d7dc161dc
Andrew White <aeewhite@gmail.com>
2016-05-14 00:09:30 -0400

    Add module object code

---
+++ ModuleManager/module.js
@@ -0,0 +1,45 @@
+var logger		= require('./logging.js'),
+	execFile	= require('child_process').execFile,
+	path 		= require('path'),
+	fs 			= require('fs');
+
+function Module(modulePath){
+	var moduleFile = JSON.parse(fs.readFileSync(modulePath + "/module/module.json", 'utf8'));
+	this.moduleName = moduleFile.name;
+	this.path = modulePath;
+	this.executable = modulePath + "/module/" + moduleFile.executable;
+	this.version = moduleFile.version;
+	this.actions = moduleFile.actions;
+	this.moduleFile = moduleFile;
+	logger.info("Loaded " + this.moduleName);
+
+	//Call the executable with message as the first argument
+	this.sendMessage = sendMessage;
+}
+
+/**
+ * Call the executable with message as the first argument
+ * @param  {String} message argument to call executable with
+ * @return {int}	return code
+ */
+function sendMessage(message){
+	var module = this;
+	logger.debug("[=>" + module.moduleName + "] Sending '" + message + "' to " + module.moduleName);
+	var child = execFile(this.executable, [message], {
+		cwd: this.path
+	});
+	
+	child.stdout.on('data',function(data){
+		data.trim().split(/\r?\n/).forEach(function(value, i){	//Split module outputs to make log look better
+			logger.info("[" + module.moduleName + "] " + value.trim());
+		});
+		
+	});
+	child.stderr.on('data',function(data){
+		data.trim().split(/\r?\n/).forEach(function(value, i){
+			logger.error("[" + module.moduleName + "] " + value.trim());
+		});
+	});
+}
+
+module.exports.Module = Module;

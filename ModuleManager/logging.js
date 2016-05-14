e444cdd58825d932d252e7eeecbc2ce56f2caa78
Andrew White <aeewhite@gmail.com>
2016-05-14 00:09:47 -0400

    Add sane dates to the log files

--- ModuleManager/logging.js
+++ ModuleManager/logging.js
@@ -3,11 +3,11 @@ var winston = require('winston');
 var logger = new (winston.Logger)({
   transports: [
     new (winston.transports.Console)({ json: false, timestamp: function(){return new Date().toLocaleString();}, level:'silly'}),
-    new winston.transports.File({ filename: __dirname + '/debug.log', json: false })
+    new winston.transports.File({ filename: __dirname + '/debug.log', json: false, timestamp: function(){return new Date().toLocaleString();} })
   ],
   exceptionHandlers: [
     new (winston.transports.Console)({ json: false, timestamp: function(){return new Date().toLocaleString();}}),
-    new winston.transports.File({ filename: __dirname + '/exceptions.log', json: false })
+    new winston.transports.File({ filename: __dirname + '/exceptions.log',timestamp: function(){return new Date().toLocaleString();}, json: false })
   ],
   exitOnError: false
 });

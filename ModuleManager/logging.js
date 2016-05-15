477dbe361bda63d115c0fe7b97bc5364c357d8ee
Andrew White <aeewhite@gmail.com>
2016-05-15 14:26:12 -0400

    Clean up logging setup

--- ModuleManager/logging.js
+++ ModuleManager/logging.js
@@ -1,13 +1,21 @@
 var winston = require('winston');
 
+var options = {
+	json: false,
+	timestamp: function(){return new Date().toLocaleString();},
+	level:'silly',
+	humanReadableUnhandledException: true,
+	colorize: true
+};
+
 var logger = new (winston.Logger)({
   transports: [
-    new (winston.transports.Console)({ json: false, timestamp: function(){return new Date().toLocaleString();}, level:'silly'}),
-    new winston.transports.File({ filename: __dirname + '/debug.log', json: false, timestamp: function(){return new Date().toLocaleString();} })
+    new (winston.transports.Console)(options),
+    new winston.transports.File({ filename: __dirname + '/debug.log', json: false, timestamp: function(){return new Date().toLocaleString();},humanReadableUnhandledException: true })
   ],
   exceptionHandlers: [
-    new (winston.transports.Console)({ json: false, timestamp: function(){return new Date().toLocaleString();}}),
-    new winston.transports.File({ filename: __dirname + '/exceptions.log',timestamp: function(){return new Date().toLocaleString();}, json: false })
+    new (winston.transports.Console)(options),
+    new winston.transports.File({ filename: __dirname + '/exceptions.log',timestamp: function(){return new Date().toLocaleString();}, json: false,humanReadableUnhandledException: true })
   ],
   exitOnError: false
 });
